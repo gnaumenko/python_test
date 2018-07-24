b4fb188ffa9ee64f207a254e964fd2c603edd519
g_naumenko <jufik@bk.ru>
2018-07-24 12:44:38 +0300

    Поправочка високосного года

--- year.py
+++ year.py
@@ -7,7 +7,7 @@
 year = (input('input year: '))
 if year % 400 == 0 or year % 4 == 0 and year % 100 != 0:
     print ("Yes")
-elif year % 100 == 0 and year % 400 != 0:
+elif year % 100 == 0 and year % 400 != 0 and year % 4 != 0:
     print ("No")
 elif year % 100 == 0:
     print ("No")
