e94deb5d666d6aa7476eb1709e93fec9d91af969
g_naumenko <jufik@bk.ru>
2018-07-24 12:39:17 +0300

    Обновлены уловия высокосного года

--- year.py
+++ year.py
@@ -1,7 +1,13 @@
-import math
-year = (input('input year: '))
-if math.fmod(year, 4):
+#import math
+#if math.fmod(year, 4):
 #if year % 4 != 0:
-    print ("No")
-else:
+#    print ("No")
+#else:
+#    print ("Yes")
+year = (input('input year: '))
+if year % 400 == 0 or year % 4 == 0 and year % 100 != 0:
     print ("Yes")
+elif year % 100 == 0 and year % 400 != 0:
+    print ("No")
+elif year % 100 == 0:
+    print ("No")
