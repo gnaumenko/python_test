f35c02106981f64c405a94dd9b9776e1be52fec1
g_naumenko <jufik@bk.ru>
2018-07-25 13:19:41 +0300

    код через диапозоны

--- Season.py
+++ Season.py
@@ -1,11 +1,27 @@
 season = float(input('input number: '))
-if season in [1, 2, 12]:
+#a = str
+#if season in [1, 2, 12]:
+#    print ('Winter')
+#elif season in [3, 4, 5]:
+#    print ('Spring')
+#elif season in [6, 7, 8]:
+#    print ('Summer')
+#elif season in [9, 10, 11]:
+#    print ('Autumn')
+#else:
+#    print ('O NOOO')
+
+if season in range(1,3) or season == 12:
     print ('Winter')
-elif season in [3, 4, 5]:
+elif season in range(3,6):
     print ('Spring')
-elif season in [6, 7, 8]:
+elif season in range(6,9):
     print ('Summer')
-elif season in [9, 10, 11]:
+elif season in range(9,12):
     print ('Autumn')
+#elif season == a:
+#    print ('O')
 else:
-    print ('O NOOO')
+    print ('Enter correct number')
+
+#как добавить обработку строки
