e64cc7a6cfeaa7b7d0408e28afd984b62a6d7fe5
g_naumenko <jufik@bk.ru>
2018-07-26 16:49:39 +0300

    lesson2

---
+++ 2.py
@@ -0,0 +1,12 @@
+#my_list = [1, 2, 3, 4, 5, 10]
+#print(text.format(x))\
+
+x = 1
+while x < 5:
+    print('i')
+    x = x + 1
+    if x == 3:
+        break
+print('out')
+
+for x in range(1, 10):
---
+++ lesson2.py
@@ -0,0 +1,20 @@
+string = 'hello {}'
+#string.forma("ju")
+game = 'ddd'
+print (string.format('ju'))
+
+print game.upper()
+print (game.lower)
+
+if game == raw.input('print').lower():
+    print('qqq')
+
+print (string.split(' ')) #razdelyaet po probely i sozdaet spisok
+print range(1, 10)
+print range(1, 10, 2)
+print range(10, 1, -2)
+
+my_list = [1, 2, 3, 4, 5, 6]
+for x in my_list:
+    print ('!')
+print ('vne cikla')
