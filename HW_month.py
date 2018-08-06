9fad9fce0a4dab4d19cce3f27beb87da33231b1b
g_naumenko <jufik@bk.ru>
2018-08-06 22:46:37 +0300

    Изменения 1

---
+++ HW_month.py
@@ -0,0 +1,6 @@
+month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+day = range(1, 31, 1)
+day_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+for x in month:
+    print("---- " + x + " ----")
