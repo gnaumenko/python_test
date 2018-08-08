9eb0d3fa5aa35f7c5f6b0d8b29b5337c63dc2a61
g_naumenko <jufik@bk.ru>
2018-08-08 22:48:37 +0300

    Изменения 2

--- HW_month.py
+++ HW_month.py
@@ -1,6 +1,14 @@
-month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-day = range(1, 31, 1)
-day_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+#day = range(1, 31, 1)
+week_days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-for x in month:
-    print("---- " + x + " ----")
+for month in months:
+    print("---- " + month + " ----")
+    for day in range(1,32):
+        if day != 32:
+            for week_day in range(1,8):
+                if week_day%7 != 1:
+                    continue
+                print (str(day) + " of " + month + "," + week_days[week_day])
+
+
